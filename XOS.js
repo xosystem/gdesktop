16e83f62d367dcb63266a85367b41b52620e18ca
xosystem <info@xosystem.org>
2018-03-18 11:35:22 +0100

    modifica

--- XOS.js
+++ XOS.js
@@ -20,7 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
  
-/* fine */
+/* fine  modifica */
 
 var DEVICE_WITH_TOUCH_EVENTS = "ontouchstart" in window;
 
